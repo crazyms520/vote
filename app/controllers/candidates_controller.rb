d1d2afa48d6fc503c8d4967dd1831afa0cfb5565
HankChang <bsuysheep927@gmail.com>
2019-07-09 23:21:21 +0800

    update create action

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -4,9 +4,26 @@ class CandidatesController < ApplicationController
     end
 
     def new
+        @candidate = Candidate.new
     end
 
     def create
-        render plain: params[:candidate]
+        
+        @candidate = Candidate.new candidate_params
+        
+        if @candidate.save
+            #success 
+            flash[:notice] = "Candidate created!"
+            redirect_to '/candidates'
+        else
+            #NG
+            render plain: "error"
+        end
+        # render plain: params[:candidate]
+    end
+
+    private
+    def candidate_params
+        params.require(:candidate).permit(:name, :party, :age, :politics)
     end
 end
